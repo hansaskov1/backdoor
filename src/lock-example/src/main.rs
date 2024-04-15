58fea4e9351f9dafad1f47d538a459e69a6977ea
hansaskov <hansaskov1999@gmail.com>
2024-04-15 13:24:23 +0200

    Incrimentilly add code

--- src/lock-example/src/main.rs
+++ src/lock-example/src/main.rs
@@ -1,111 +1,5 @@
-
-/*
-use fluent_state_machine::StateMachineBuilder;
-use lock_example::component::Component;
-use lock_example::sensor_states::{DoorState, LockState};
-use std::time::{Duration, Instant};
-
-use esp_idf_hal::delay::FreeRtos;
-use esp_idf_hal::gpio::*;
-use esp_idf_hal::peripherals::Peripherals;
-
-type Lock<'a> = Component<'a, LockState, Gpio18, Gpio33>;
-type Door<'a> = Component<'a, DoorState, Gpio4, Gpio14>;
-
-#[derive(Debug, Clone, Copy, PartialEq)]
-enum States {
-    Locked,
-    Unlocked,
-    Locking,
-    Unlocking,
-}
-
-#[derive(PartialEq)]
-enum Event {
-    OpenDoor,
-    Step,
-}
-
-struct Store<'a> {
-    lock: Lock<'a>,
-    door: Door<'a>,
-    duration_in_state: Instant,
-}
-
-
-fn main() -> anyhow::Result<()> {
-    esp_idf_hal::sys::link_patches();
-
-    // Bind the log crate to the ESP Logging facilities
-    esp_idf_svc::log::EspLogger::initialize_default();
-
-    log::info!("Starting up!");
-
-    // Mandetory for ESP-IDF to work.
-    let peripherals = Peripherals::take()?;
-
-    // Values will be accesable within the state machine.
-    let store = Store {
-        lock: Component::new(peripherals.pins.gpio18, peripherals.pins.gpio33)?,
-        door: Component::new(peripherals.pins.gpio4, peripherals.pins.gpio14)?,
-        duration_in_state: Instant::now(),
-    };
-
-    // Function that will be called on every state change.
-    let global_action = |store: &mut Store, state: &States, _: &Event| {
-        // Reset the duration in state.
-        store.duration_in_state = Instant::now();
-
-        // Log when the state changes
-        log::info!("State: {:?}", state);
-    };
-
-    // Construct state machine.
-    let mut state_machine = StateMachineBuilder::new(store, States::Locked)
-        .set_global_action(global_action)
-        .state(States::Locked)
-            .on(Event::OpenDoor)
-                .go_to(States::Unlocking)
-        .state(States::Unlocking)
-            .on(Event::Step)
-                .go_to(States::Unlocked)
-                .only_if(|store| store.lock.state == LockState::Unlocked)
-            .on(Event::Step)
-                .go_to(States::Locked)
-                .only_if(|store| store.duration_in_state.elapsed() > Duration::from_secs(10) && store.door.state == DoorState::Closed)
-        .state(States::Unlocked)
-            .on(Event::Step)
-                .go_to(States::Locking)
-                .only_if(|store| {
-                    store.door.state == DoorState::Closed
-                    && store.duration_in_state.elapsed() > Duration::from_secs(5)
-                })
-        .state(States::Locking)
-            .on(Event::Step)
-                .go_to(States::Locked)
-                .only_if(|store| store.lock.state == LockState::Locked)
-        .build()
-        .unwrap();
-
-
-    log::info!("State: {:?}", state_machine.state);
-    state_machine.trigger(Event::OpenDoor);
-
-
-    // Our main runtime
-    loop {
-        FreeRtos::delay_ms(10);
-
-        state_machine.store.lock.step()?;
-        state_machine.store.door.step()?;
-        state_machine.trigger(Event::Step);
-    }
-}
-*/
-
-
 use core::convert::TryInto;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 use embedded_svc::wifi::{AuthMethod, ClientConfiguration, Configuration};
 
 use esp_idf_hal::gpio::{Gpio14, Gpio18, Gpio33, Gpio4};
@@ -115,6 +9,7 @@ use esp_idf_svc::log::EspLogger;
 use esp_idf_svc::mqtt::client::{EspMqttClient, EspMqttConnection, MqttClientConfiguration, QoS};
 use esp_idf_svc::wifi::{BlockingWifi, EspWifi};
 use esp_idf_svc::{eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition};
+use fluent_state_machine::{StateMachine, StateMachineBuilder};
 use lock_example::component::Component;
 use lock_example::sensor_states::{DoorState, LockState};
 use log::info;
@@ -133,6 +28,13 @@ enum States {
 #[derive(PartialEq)]
 enum Event {
     OpenDoor,
+    Step
+}
+
+struct Store<'a> {
+    lock: Lock<'a>,
+    door: Door<'a>,
+    duration_in_state: Instant,
 }
 
 
@@ -152,8 +54,8 @@ fn main() -> anyhow::Result<()> {
     let sys_loop = EspSystemEventLoop::take()?;
     let nvs = EspDefaultNvsPartition::take()?;
 
-    let lock: Lock = Component::new(peripherals.pins.gpio18, peripherals.pins.gpio33)?;
-    let door: Door = Component::new(peripherals.pins.gpio4, peripherals.pins.gpio14)?;
+    let mut lock: Lock = Component::new(peripherals.pins.gpio18, peripherals.pins.gpio33)?;
+    let mut door: Door = Component::new(peripherals.pins.gpio4, peripherals.pins.gpio14)?;
 
     // Configure Wifi
     let mut wifi = BlockingWifi::wrap(
@@ -172,7 +74,7 @@ fn main() -> anyhow::Result<()> {
     )?;
 
     // Run event loop
-    run(&mut mqtt_client, &mut mqtt_conn, lock, door, MQTT_TOPIC)?;
+    run(&mut mqtt_client, &mut mqtt_conn, MQTT_TOPIC, lock, door)?;
     Ok(())
 }
 
@@ -180,14 +82,58 @@ fn main() -> anyhow::Result<()> {
 fn run(
     client: &mut EspMqttClient<'_>,
     connection: &mut EspMqttConnection,
-    lock: Lock,
-    door: Door,
     topic: &str,
+    lock: Lock,
+    door: Door
 ) -> Result<(), EspError> {
     std::thread::scope(|scope| {
         let message = "Hello from esp!";
         let message_bytes = message.as_bytes();
         let sleep_duration = Duration::from_secs(2);
+    
+        // Values will be accesable within the state machine.
+        let store = Store {
+            lock,
+            door: door, 
+            duration_in_state: Instant::now(),
+        };
+
+        let global_action = |store: &mut Store, state: &States, _: &Event| {
+            // Reset the duration in state.
+            store.duration_in_state = Instant::now();
+    
+            // Log when the state changes
+            log::info!("State: {:?}", state);
+        };
+    
+        let mut state_machine = StateMachineBuilder::new(store, States::Locked)
+            .set_global_action(global_action)
+            .state(States::Locked)
+                .on(Event::OpenDoor)
+                    .go_to(States::Unlocking)
+            .state(States::Unlocking)
+                .on(Event::Step)
+                    .go_to(States::Unlocked)
+                    .only_if(|store| store.lock.state == LockState::Unlocked)
+                .on(Event::Step)
+                    .go_to(States::Locked)
+                    .only_if(|store| {
+                        store.duration_in_state.elapsed() > Duration::from_secs(10) 
+                        && store.door.state == DoorState::Closed})
+            .state(States::Unlocked)
+                .on(Event::Step)
+                    .go_to(States::Locking)
+                    .only_if(|store| {
+                        store.door.state == DoorState::Closed
+                        && store.duration_in_state.elapsed() > Duration::from_secs(5)
+                    })
+            .state(States::Locking)
+                .on(Event::Step)
+                    .go_to(States::Locked)
+                    .only_if(|store| store.lock.state == LockState::Locked)
+            .build()
+            .unwrap();
+
 
         std::thread::Builder::new()
             .stack_size(6000)
@@ -196,12 +142,14 @@ fn run(
             })
             .unwrap();
 
+
         client.subscribe(topic, QoS::AtMostOnce)?;
         std::thread::sleep(Duration::from_millis(500));
 
         loop {
-            client.enqueue(topic, QoS::AtMostOnce, false, message_bytes)?;
-            info!("Sent message: {}", message);
+            state_machine.store.lock.step().unwrap();
+            state_machine.store.door.step().unwrap();
+            state_machine.trigger(Event::Step);
             std::thread::sleep(sleep_duration);
         }
     })
